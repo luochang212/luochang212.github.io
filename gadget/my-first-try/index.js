defc5edba0f6401c673576592fd685e560cebeba
Chang Luo <luochang212@gmail.com>
2019-11-17 21:21:58 +0800

    update

---
+++ gadget/my-first-try/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+class Hello extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { opacity: 1.0 };
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(function () {
+            var opacity = this.state.opacity;
+            opacity -= .05;
+            if (opacity < 0.1) {
+                opacity = 1.0;
+            }
+            this.setState({
+                opacity: opacity
+            });
+        }.bind(this), 100);
+    }
+
+    render() {
+        return (
+            <div style={{ opacity: this.state.opacity }}>
+                Hello {this.props.name}
+            </div>
+        );
+    }
+}
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(
+    <Hello name="world" />,
+    domContainer
+);
